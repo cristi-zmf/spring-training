502972af1dfd9766c4ac9d3eec3d128b5f394bf2
cristi-zmf <cristi>
2020-07-09 00:12:24 +0300

    Fixed test for exercise 2

--- src/test/java/com/cristi/simple/spring/simplespring/exercises/ex2/ComputerLocalIT.java
+++ src/test/java/com/cristi/simple/spring/simplespring/exercises/ex2/ComputerLocalIT.java
@@ -2,9 +2,10 @@ package com.cristi.simple.spring.simplespring.exercises.ex2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@SpringBootTest
 class ComputerLocalIT {
     @Autowired
     private Computer smartComputer;
